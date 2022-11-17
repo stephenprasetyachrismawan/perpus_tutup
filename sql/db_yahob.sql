a6c14247374dd07a0445a80f31288bc6e8738bb7
irfanp22 <106128747+irfanp22@users.noreply.github.com>
2022-11-17 20:14:12 +0700

    commit

--- sql/db_yahob.sql
+++ sql/db_yahob.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 02, 2022 at 03:39 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Nov 17, 2022 at 02:13 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,9 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `password` varchar(12) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `nama`, `password`) VALUES
+(1, 'Graham', 'Graham', '$2y$10$Z0BkhKGUjoE03lRDtrsf8ePiZw6jypAZ6A3oAtg0AvrDmCZYBmCiO');
 
 -- --------------------------------------------------------
 
@@ -44,8 +52,16 @@ CREATE TABLE `buku` (
   `judul` varchar(255) NOT NULL,
   `pengarang` varchar(32) NOT NULL,
   `tahun` int(11) NOT NULL,
-  `id_jenis` int(11) NOT NULL
+  `id_jenis` int(11) NOT NULL,
+  `stok` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id`, `judul`, `pengarang`, `tahun`, `id_jenis`, `stok`) VALUES
+(17, 'Just Code', 'Prijatno', 1998, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -58,6 +74,13 @@ CREATE TABLE `jenis_buku` (
   `jenis` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `jenis_buku`
+--
+
+INSERT INTO `jenis_buku` (`id`, `jenis`) VALUES
+(1, 'Novel');
+
 -- --------------------------------------------------------
 
 --
@@ -65,14 +88,73 @@ CREATE TABLE `jenis_buku` (
 --
 
 CREATE TABLE `mahasiswa` (
-  `id` varchar(9) NOT NULL,
+  `id` int(11) NOT NULL,
+  `nim` varchar(9) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `password` varchar(12) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `password`, `email`, `no_hp`, `alamat`) VALUES
+(1, 'H1D021025', 'H1D021025', '$2y$10$iz9WtylmQVVffeqta7vKpeoM.4jJCbfvGzwx3YqO5buVP7cdNurrq', '', '', ''),
+(2, 'H1D021111', 'djoko sasana', '$2y$10$UL0th/UvWOeW.bktB57SUeJhY.M9MVi1Pjpsx6GauF2CUsKLO7AE6', '', '', ''),
+(3, 'H1D021043', 'Irfan Priatna', '$2y$10$/9A.Va2kuCUZYXdVnEkP1ONuj3KbELJl27bG9xX.LlKt34j9j1HaC', 'irfan.priatna@mhs.unsoed.ac.id', '0895375115609', 'Jalan In Aja No.333');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `id_mahasiswa` varchar(9) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tamu`
+--
+
+CREATE TABLE `tamu` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tamu`
+--
+
+INSERT INTO `tamu` (`id`, `username`, `nama`, `password`, `email`, `no_hp`, `alamat`) VALUES
+(1, 'irfan', 'irfan', '$2y$10$dh7xjUd6Efo.RX66sKbfceh6HTFUU96sLVWprodwjeGi/8plAFV4C', '', '', ''),
+(2, 'evan', 'Evans', '$2y$10$M6aD1W.p0cEdBcaNh0WETu7K9GqcatppuaqmvXJCDX4DPUo8gGOta', '', '', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `buku`
@@ -91,23 +173,63 @@ ALTER TABLE `jenis_buku`
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nim` (`nim`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_buku` (`id_buku`),
+  ADD KEY `fk_mahasiswa` (`id_mahasiswa`);
+
+--
+-- Indexes for table `tamu`
+--
+ALTER TABLE `tamu`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `jenis_buku`
 --
 ALTER TABLE `jenis_buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tamu`
+--
+ALTER TABLE `tamu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -118,6 +240,13 @@ ALTER TABLE `jenis_buku`
 --
 ALTER TABLE `buku`
   ADD CONSTRAINT `fk_jenis` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_buku` (`id`);
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `fk_buku` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`),
+  ADD CONSTRAINT `fk_mahasiswa` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`nim`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
