8f75d7c9f521a82b2e58401181acd134041caa21
irfanp22 <106128747+irfanp22@users.noreply.github.com>
2022-11-28 14:48:14 +0700

    fix dashboard

--- _home.php
+++ _home.php
@@ -1,70 +1,84 @@
+<?php
+$buku = mysqli_fetch_array(mysqli_query($koneksi, "SELECT COUNT(judul) AS jmlbuku FROM buku"));
+$anggota = mysqli_fetch_array(mysqli_query($koneksi, "SELECT COUNT(nama) AS jmlanggota FROM mahasiswa"));
+$pinjam = mysqli_fetch_array(mysqli_query($koneksi, "SELECT COUNT(id) AS pinjamprocess FROM peminjaman WHERE MONTH(tanggal_pinjam) = MONTH(current_date())  AND status != 'done'"));
+$balik = mysqli_fetch_array(mysqli_query($koneksi, "SELECT COUNT(id) AS pinjamdone FROM peminjaman WHERE MONTH(tanggal_kembali) = MONTH(current_date())  AND status = 'done'"));
+?>
 <!-- Content Row -->
 <div class="row">
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Buku</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo ""; ?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-solid fa-book fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <a href="?page=viewbuku">
+      <div class="card border-left-primary shadow h-100 py-2 zoom">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Jumlah Buku</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo number_format($buku['jmlbuku']); ?></div>
             </div>
+            <div class="col-auto">
+              <i class="fas fa-solid fa-book fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Anggota</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo ""; ?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-laugh-wink fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <a href="?page=anggota">
+      <div class="card border-left-success shadow h-100 py-2 zoom">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Jumlah Anggota</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo number_format($anggota['jmlanggota']); ?></div>
             </div>
+            <div class="col-auto">
+              <i class="fas fa-laugh-wink fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
 
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Peminjaman dalam Proses</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo "";?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-list-alt fa-2x text-gray-300 ml-1"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <!-- Earnings (Monthly) Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <a href="?page=viewpinjam">
+      <div class="card border-left-info shadow h-100 py-2 zoom">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Peminjaman dalam Proses</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo number_format($pinjam['pinjamprocess']); ?></div>
             </div>
+            <div class="col-auto">
+              <i class="fas fa-list-alt fa-2x text-gray-300 ml-1"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
 
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Jumlah Peminjaman Selesai</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo ""; ?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-list-alt fa-2x text-gray-300 ml-1"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
+  <!-- Pending Requests Card Example -->
+  <div class="col-xl-3 col-md-6 mb-4">
+    <a href="?page=viewpinjam">
+      <div class="card border-left-warning shadow h-100 py-2 zoom">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Jumlah Peminjaman Selesai</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo number_format($balik['pinjamdone']); ?></div>
             </div>
+            <div class="col-auto">
+              <i class="fas fa-list-alt fa-2x text-gray-300 ml-1"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
 </div>
--- dashboard.php
+++ dashboard.php
@@ -133,9 +133,14 @@ include "koneksi.php";
                     <div class="topbar-divider d-none d-sm-block"></div>
 
                     <!-- Nav Item - User Information -->
+                    <?php
+                        $sql = mysqli_query($koneksi, "SELECT pic FROM admin WHERE username = '" . $_SESSION['username'] . "'");
+                        $data = mysqli_fetch_array($sql);
+                    ?>
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['username'] ?></span>
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small text-capitalize"><?php echo $_SESSION['username'] ?></span>
+                            <img class="img-profile rounded-circle" src="<?php echo $data['pic'] ?>" alt="profile">
                         </a>
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
